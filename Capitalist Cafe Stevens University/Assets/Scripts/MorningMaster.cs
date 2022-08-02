aaf031c365ed682cf3970f061bc6b015564d7075
LucasCunnane1623 <86751705+LucasCunnane1623@users.noreply.github.com>
2022-08-02 01:10:20 -0400

    trying to make shit work with the npcs

---
+++ Capitalist Cafe Stevens University/Assets/Scripts/MorningMaster.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MorningMaster : MonoBehaviour
+{
+    public Collider2D NPCCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // if()
+    }
+}

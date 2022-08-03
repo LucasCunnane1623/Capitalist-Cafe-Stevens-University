10495d99f21c35672b94ed5743e8c1b515e5e810
Golden-Ne <tsaun8@gmail.com>
2022-08-02 20:49:22 -0400

    V1.2?
    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

---
+++ Capitalist Cafe Stevens University/Assets/Scripts/CafeManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CafeManager : MonoBehaviour
+{
+   
+    public static int playerStars = 0;
+  
+
+}
--- Capitalist Cafe Stevens University/Assets/Scripts/bookstore.cs
+++ Capitalist Cafe Stevens University/Assets/Scripts/bookstore.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class bookstore : MonoBehaviour
 {
+    public int starCount;
+    public TMP_Text starVal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        starCount += CafeManager.playerStars;
+        starVal.SetText(CafeManager.playerStars + "");
     }
 
     // Update is called once per frame
